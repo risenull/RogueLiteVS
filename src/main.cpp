92e1cd62bc653fbdf2dc585729766a9296e2114b
Braylon <libr2023@mountunion.edu>
2025-10-03 09:46:22 -0400

    Finally got the black window to work, required a ton of bullshit it was becauase i was using sdl2 code btw

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_video.h>
+#include <iostream>
 
+int main() {
+    std::cout << "Starting SDL3 app..." << std::endl;
 
-SDL_Window * SDL_CreateWindow(const char *title, int x, int y, int w, int h, Uint32 flags);
+    // SDL_Init in SDL3 takes an unsigned int (SDL_InitFlags)
+    if (!SDL_Init(0)) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL3 Test", 800, 600, SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
